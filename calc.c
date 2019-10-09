77baa2c260fdcef60a7ba02621e2e320b5552149
Aidan Bowman <aidan.bowman@cooper.edu>
2019-10-09 18:27:35 -0400

    calc.c

--- calc.c
+++ calc.c
@@ -1,31 +1,19 @@
  #include <stdio.h>
  #include <stdlib.h>
 
-//main()
-//{
-  //  int numb1, numb2, operant; 
-  //  numb1 = 
-  //  numb2 = 
-  //  operant = 
-
-  //  while (operant =>  )
-
-
-  //  argv[1]
-  //  for (i = 0; i < argc; ++i)
-   //         printf("")
-
-//}
-int main(int argc, **char argv){
-    printf("arg[1]: %s\n",argv[1]);
-    printf("arg[1][0]:%c\n", argv[1][0]);
+int main(int argc, char **argv){
+    double d = atof(argv[2]);
+    double dd = atof(argv[3]);
+    
     if(argv[1][0] == '+')
-        printf("Addition");
-    else if(argv[1][0] == '-')
-        printf("SUbtraction");
-    else if(argv[1][0] == '*')
-        printf("Multiplication");
-    else(argv[1][0] == '/')
-        printf("Division");
+        printf("%f\n", d + dd);
 
+    else if(argv[1][0] == '-')
+        printf("%f\n", d - dd);
+
+    else if(argv[1][0] == 'X')
+        printf("%f\n", d * dd);
+
+    else if(argv[1][0] == '/')
+        printf("%f\n", d / dd);
 }
